c10eca040a60a7d196bb4e3d83318e42bf88f230
liwenhe <411934049@qq.com>
2015-03-03 13:47:31 +0800

    init project

---
+++ src/main/java/me/graduation/interfaces/IndexController.java
@@ -0,0 +1,19 @@
+package me.graduation.interfaces;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by _liwenhe on 2015/3/3.
+ */
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping
+    public ModelAndView index() throws Exception {
+        return new ModelAndView("/index");
+    }
+
+}
